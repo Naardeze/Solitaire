387792e9e585b1aa3a3a2161c7453e3f2eda0748
Naardeze <141658644+Naardeze@users.noreply.github.com>
2024-10-12 19:57:46 +0200

    Add files via upload

---
+++ Pile.java
@@ -0,0 +1,28 @@
+package solitaire;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+
+public class Pile extends JLabel {
+    
+    public Pile() {}
+
+    public Pile(String symbol) {
+        super(symbol, JLabel.CENTER);
+        
+        setForeground(new Color(-16743936));
+    }
+
+    public Card getCard() {
+        return (Card) getComponent(0);
+    }
+    
+    public void setCard(Card card) {
+        add(card, 0);
+    }
+    
+    public boolean isEmpty() {
+        return getComponentCount() == 0;
+    }
+    
+}
