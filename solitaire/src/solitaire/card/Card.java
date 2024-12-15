c8b2da0478eb69d63492385cc0658f68d81c1990
Naardeze <141658644+Naardeze@users.noreply.github.com>
2024-12-15 22:10:06 +0100

    Update Card.java

--- solitaire/src/solitaire/card/Card.java
+++ solitaire/src/solitaire/card/Card.java
@@ -10,7 +10,6 @@ final public class Card extends JLabel {
     final public static int CARD_HEIGHT = 60;
     
     final private static ImageIcon BACK = new ImageIcon("back.png");
-    
     public static BufferedImage face;
     
     public enum Suit {
